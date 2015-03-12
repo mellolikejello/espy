4d948c7238d31c18734c74a5f4c4078e550b94f6
Melody <mgk1068@rit.edu>
2015-03-12 00:08:56 -0400

    updated nav and added test views

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,8 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('HomeCtrl', function($scope) {})
+
+.controller('MapCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
